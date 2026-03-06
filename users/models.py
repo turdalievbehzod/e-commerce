ee384a1233e68c51a7e603b44cd934e49df501a9
turdalievbehzod <turdalievbehzod58@gmail.com>
2026-03-06 16:46:18 +0500

    user model fixed

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=20, blank=True)
-    avatar = models.ImageField(upload_to="users/avatars/", blank=True)
+    profile_photo = models.ImageField(upload_to="users/profile_photo/", blank=True)
 
     def __str__(self) -> str:
         return self.get_full_name() or self.username
